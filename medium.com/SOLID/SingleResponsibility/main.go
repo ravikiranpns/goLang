85bf6a0a79ddc7e5bab3557e4dd217277dd96df0
Apple <apple@Apples-MacBook-Pro.local>
2022-06-30 15:59:22 +0530

    1701100.30.06.22 - SOLID - SRP : Divided the Area calculation and Printing wiht new shape interface and outPrinter empty struct

--- medium.com/SOLID/SingleResponsibility/main.go
+++ medium.com/SOLID/SingleResponsibility/main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+/*
 //type circle with its area method
 type circle struct {
 	radius float32
@@ -28,4 +29,45 @@ func main() {
 
 	s := square{sideLen: 2}
 	s.area()
+}
+*/
+
+//type circle with its area method
+type circle struct {
+	radius float32
+}
+
+func (c circle) area() float32 {
+	return math.Pi * c.radius * c.radius
+}
+
+type square struct {
+	sideLen float32
+}
+
+func (s square) area() float32 {
+	return s.sideLen * s.sideLen
+}
+
+type shape interface {
+	area() float32
+}
+
+type outPrinter struct{}
+
+func (op outPrinter) toText(s shape) string {
+	return fmt.Sprintf("The Area is : %f", s.area())
+}
+
+func main() {
+	c := circle{radius: 3}
+	c.area()
+
+	s := square{sideLen: 2}
+	s.area()
+
+	out := outPrinter{}
+
+	fmt.Println(out.toText(c))
+	fmt.Println(out.toText(s))
 }
