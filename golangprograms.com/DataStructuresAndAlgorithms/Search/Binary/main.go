fe46db22c27ffc178a7ddac296e9d15f1e9b656b
Ravikira P. Sattigeri <ravisattigeri@gmail.com>
2022-07-28 18:16:21 +0530

    1742500: DS&ALGO - Binary Search.

---
+++ golangprograms.com/DataStructuresAndAlgorithms/Search/Binary/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func binarySearch(key int, slc []int) bool {
+	low := 0
+	high := len(slc) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if slc[mid] < key {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	if low == len(slc) || slc[low] != key {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	items := []int{1, 2, 3, 5, 7, 8, 9, 10}
+	fmt.Println(binarySearch(7, items))
+}
