ab83c1f908efe84f1e6896608b07415b2ff729e2
Ravikiranpns <ravisattigeri@gmail.com>
2022-10-03 18:29:05 +0530

    1820400:GBE - Interface example

---
+++ gobyexample.com/Interface/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// https://gobyexample.com/interfaces
+
+type geometry interface {
+	area() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	//fmt.Println(g.are
+}
+
+func main() {
+	fmt.Println("The interface")
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+
+	measure(r)
+	measure(c)
+}
