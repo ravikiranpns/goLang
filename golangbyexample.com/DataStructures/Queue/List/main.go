5a6bb0de1305677006f8ff3d7868f517ce695a22
Ravikiranpns <ravisattigeri@gmail.com>
2023-11-05 17:54:41 +0530

    2071900:List Queue Front func

--- golangbyexample.com/DataStructures/Queue/List/main.go
+++ golangbyexample.com/DataStructures/Queue/List/main.go
@@ -18,5 +18,15 @@ func (q *LQ) Dequeue() error {
 		ele := q.lq.Front()
 		q.lq.Remove(ele)
 	}
-	return fmt.Errorf("Pop Error: Queue is empty")
+	return fmt.Errorf("pop error: Queue is empty")
+}
+
+func (q *LQ) Front() (string, error) {
+	if q.lq.Len() > 0 {
+		if val, ok := q.lq.Front().Value.(string); ok {
+			return val, nil
+		}
+		return "", fmt.Errorf("peep Error: Queue datatype is incorrect")
+	}
+	return "", fmt.Errorf("peep Error: Queue is empty")
 }
