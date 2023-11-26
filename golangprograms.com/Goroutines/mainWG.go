4be7216e255ecf4fb5859523b725c14892f1fe0c
Ravikiranpns <ravisattigeri@gmail.com>
2023-11-26 17:04:56 +0530

    Goroutine with WaitGroup of sync, http.Get of net/http, ReadAll of io

--- golangprograms.com/Goroutines/mainWG.go
+++ golangprograms.com/Goroutines/mainWG.go
@@ -5,10 +5,18 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
+// WaitGroup is used to wait for the program to finish goroutines.
+var wg sync.WaitGroup
+
 func responseSize(url string) {
+
+	// Schedule the call to WaitGroup's Done to tell goroutine is completed
+	defer wg.Done()
+
 	fmt.Println("S1: ", url)
 	response, err := http.Get(url)
 	if err != nil {
@@ -27,7 +35,15 @@ func responseSize(url string) {
 }
 
 func main() {
+
+	// Add a count, one for each goroutine.
+
+	wg.Add(2)
 	go responseSize("https://www.golangprograms.com")
 	go responseSize("https://coderwall.com")
 	time.Sleep(2 * time.Second)
+
+	wg.Wait()
+
+	fmt.Println("Terminating main")
 }
