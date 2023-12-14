9ddd38e1ab28fb5d1f33bc93229485c88d5e04e3
Ravikiranpns <ravisattigeri@gmail.com>
2023-12-14 09:20:13 +0530

    Generics : commented error statements.

--- gobyexample.com/Generics/main.go
+++ gobyexample.com/Generics/main.go
@@ -2,8 +2,6 @@
 // ONS - 1822600.06.10.22
 package main
 
-import "fmt"
-
 /* Starting with version 1.18, Go added Support for
 generics, also known as type parameters.
 
@@ -71,51 +69,51 @@ the compiler infers them automatically.
 	}
 */
 
-func MapKeys[K comparable, V any](m map[K]V) []K {
-	r := make([]K, 0, len(m))
-	for k := range m {
-		r = append(r, k)
-	}
-	return r
-}
+// func MapKeys[K comparable, V any](m map[K]V) []K {
+// 	r := make([]K, 0, len(m))
+// 	for k := range m {
+// 		r = append(r, k)
+// 	}
+// 	return r
+// }
 
-type List[T any] struct {
-	head, tail *element[T]
-}
+// type List[T any] struct {
+// 	head, tail *element[T]
+// }
 
-type element[T any] struct {
-	next *element[T]
-	val  T
-}
+// type element[T any] struct {
+// 	next *element[T]
+// 	val  T
+// }
 
-func (lst *List[T]) Push(v T) {
-	if lst.tail == nil {
-		lst.head = &element[T]{val: v}
-		lst.tail = lst.head
-	} else {
-		lst.tail.next = &element[T]{val: v}
-		lst.tail = lst.tail.next
-	}
-}
+// func (lst *List[T]) Push(v T) {
+// 	if lst.tail == nil {
+// 		lst.head = &element[T]{val: v}
+// 		lst.tail = lst.head
+// 	} else {
+// 		lst.tail.next = &element[T]{val: v}
+// 		lst.tail = lst.tail.next
+// 	}
+// }
 
-func (lst *List[T]) GetAll() []T {
-	var elems []T
-	for e := lst.head; e != nil; e = e.next {
-		elems = append(elems, e.val)
-	}
-	return elems
-}
+// func (lst *List[T]) GetAll() []T {
+// 	var elems []T
+// 	for e := lst.head; e != nil; e = e.next {
+// 		elems = append(elems, e.val)
+// 	}
+// 	return elems
+// }
 
 func main() {
-	var m = map[int]string{1: "2", 2: "4", 4: "8"}
+	// var m = map[int]string{1: "2", 2: "4", 4: "8"}
 
-	fmt.Println("keys: ", MapKeys(m))
+	// fmt.Println("keys: ", MapKeys(m))
 
-	_ = MapKeys[int, string](m)
+	// _ = MapKeys[int, string](m)
 
-	lst := List[int]{}
-	lst.Push(10)
-	lst.Push(13)
-	lst.Push(23)
-	fmt.Println("list: ", lst.GetAll())
+	// lst := List[int]{}
+	// lst.Push(10)
+	// lst.Push(13)
+	// lst.Push(23)
+	// fmt.Println("list: ", lst.GetAll())
 }
