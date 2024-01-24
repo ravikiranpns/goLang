55f2210937235eebbe4154df341e4440a3cc5006
Ravikiranpns <ravisattigeri@gmail.com>
2024-01-24 17:00:49 +0530

    ProductOfArrayExceptSelf - prefix and postfix logic

--- root/neetcode.com/ArrayHash/ProductOfArrayExceptSelf/main.go
+++ root/neetcode.com/ArrayHash/ProductOfArrayExceptSelf/main.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+
+	prefix := 1
+	for i, num := range nums {
+		res[i] = prefix
+		prefix *= num
+	}
+
+	postfix := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] *= postfix
+		postfix *= nums[i]
+	}
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(productExceptSelf(nums))
+}
